fdc0262e2940ed9e897a1ad10265d9af617daf8e
Akash Kwatra <akashkw@gmail.com>
2018-06-03 18:20:10 -0500

    add fugitive and helptext

--- plugin_env/env_setup.sh
+++ plugin_env/env_setup.sh
@@ -14,18 +14,9 @@ git pull
 # Clear out old environment
 rm -rf ~/.vim ~/.vimrc ~/.tmux.conf
 
-# Install Pathogen
-mkdir -p ~/.vim/autoload ~/.vim/bundle
-curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
-
-# Install Airline
-git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
-
-# Install Airline Themes
-git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
-
-# Install Tmux Airline
-git clone https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline.vim
+# Create Config files
+cp vimrc ~/.vimrc
+cp tmux.conf ~/.tmux.conf
 
 # Install Powerline Fonts
 wget https://github.com/powerline/powerline/raw/develop/font/PowerlineSymbols.otf
@@ -36,7 +27,23 @@ wget https://github.com/powerline/powerline/raw/develop/font/10-powerline-symbol
 mkdir -p ~/.config/fontconfig/conf.d
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
 
-# Create Config files
-cp vimrc ~/.vimrc
-cp tmux.conf ~/.tmux.conf
+# Install Pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install Airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+vim -u NONE -c "helptags ~/.vim/bundle/vim-airline/doc" -c q
+
+# Install Airline Themes
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+vim -u NONE -c "helptags ~/.vim/bundle/vim-airline-themes/doc" -c q
+
+# Install Tmux Airline
+git clone https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline.vim
+vim -u NONE -c "helptags ~/.vim/bundle/tmuxline.vim/doc" -c q
+
+# Install Fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+vim -u NONE -c "helptags ~/.vim/bundle/vim-fugitive/doc" -c q
 
