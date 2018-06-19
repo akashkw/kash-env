26235f57fa01e5990e6f76fd3f7242831ad8984a
Akash Kwatra <akashkw@gmail.com>
2018-06-18 23:47:48 -0500

    add vim plugins

--- mac_env/env_setup.sh
+++ mac_env/env_setup.sh
@@ -49,3 +49,43 @@ sudo rm -rf fonts
 # Update iterm2 preferences
 sudo rm ~/Library/Preferences/com.googlecode.iterm2.plist
 sudo cp com.googlecode.iterm2.plist ~/Library/Preferences
+
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install colorschemes
+git clone  https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/colorschemes
+curl https://raw.githubusercontent.com/nightsense/wonka/master/colors/wonka-dark.vim > ~/.vim/bundle/colorschemes/colors/wonka-dark.vim
+
+# Install airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+vim -u NONE -c "helptags ~/.vim/bundle/vim-airline/doc" -c q
+
+# Install airline-themes
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+vim -u NONE -c "helptags ~/.vim/bundle/vim-airline-themes/doc" -c q
+
+# Install tmuxline
+git clone https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline.vim
+vim -u NONE -c "helptags ~/.vim/bundle/tmuxline.vim/doc" -c q
+
+# Install fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+vim -u NONE -c "helptags ~/.vim/bundle/vim-fugitive/doc" -c q
+
+# Install nerdcommenter
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+vim -u NONE -c "helptags ~/.vim/bundle/nerdcommenter/doc" -c q
+
+# Install syntastic
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
+vim -u NONE -c "helptags ~/.vim/bundle/syntastic/doc" -c q
+
+# Install easymotion
+git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/vim-easymotion
+vim -u NONE -c "helptags ~/.vim/bundle/vim-easymotion/doc" -c q
+
+# Log out
+killall iTerm2 Terminal
+sudo pkill loginwindow
