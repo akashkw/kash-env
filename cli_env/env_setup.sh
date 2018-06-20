4183c74d57c99cea01c03e707867dba487d24913
Akash Kwatra <akashkw@gmail.com>
2018-06-20 15:20:00 -0500

    initial cli env commit

--- cli_env/env_setup.sh
+++ cli_env/env_setup.sh
@@ -6,22 +6,54 @@ cd $SCRIPT_PATH
 
 # Install dependencies
 sudo apt-get update -y && sudo apt-get upgrade -y
-sudo apt-get install -y curl git vim tmux
+sudo apt-get install -y curl git vim tmux apt-transport-https 
 
 # Get newest config data
 git pull
 
 # Clear out old environment
-rm -rf ~/.vim ~/.vimrc ~/.tmux.conf
+rm -rf ~/.vim ~/.vimrc ~/.tmux.conf 
 
-# Set primary editor to vim in bashrc
-sed -i '/export VISUAL/d' ~/.bashrc
-sed -i '/export EDITOR/d' ~/.bashrc
-echo export VISUAL=vim >> ~/.bashrc
-echo export EDITOR=vim >> ~/.bashrc
-git config --global core.editor "vim"
-
-# Create Config files
+# Create config files
 cp vimrc ~/.vimrc
 cp tmux.conf ~/.tmux.conf
+
+# Set primary editor to vim in git
+git config --global core.editor "vim"
+
+# Install pathogen
+mkdir -p ~/.vim/autoload ~/.vim/bundle
+curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+
+# Install colorschemes
+git clone  https://github.com/flazz/vim-colorschemes.git ~/.vim/bundle/colorschemes
+curl https://raw.githubusercontent.com/nightsense/wonka/master/colors/wonka-dark.vim > ~/.vim/bundle/colorschemes/colors/wonka-dark.vim
+
+# Install airline
+git clone https://github.com/vim-airline/vim-airline ~/.vim/bundle/vim-airline
+vim -u NONE -c "helptags ~/.vim/bundle/vim-airline/doc" -c q
+
+# Install airline-themes
+git clone https://github.com/vim-airline/vim-airline-themes ~/.vim/bundle/vim-airline-themes
+vim -u NONE -c "helptags ~/.vim/bundle/vim-airline-themes/doc" -c q
+
+# Install tmuxline
+git clone https://github.com/edkolev/tmuxline.vim ~/.vim/bundle/tmuxline.vim
+vim -u NONE -c "helptags ~/.vim/bundle/tmuxline.vim/doc" -c q
+
+# Install fugitive
+git clone https://github.com/tpope/vim-fugitive.git ~/.vim/bundle/vim-fugitive
+vim -u NONE -c "helptags ~/.vim/bundle/vim-fugitive/doc" -c q
+
+# Install nerdcommenter
+git clone https://github.com/scrooloose/nerdcommenter.git ~/.vim/bundle/nerdcommenter
+vim -u NONE -c "helptags ~/.vim/bundle/nerdcommenter/doc" -c q
+
+# Install syntastic
+git clone --depth=1 https://github.com/vim-syntastic/syntastic.git ~/.vim/bundle/syntastic
+vim -u NONE -c "helptags ~/.vim/bundle/syntastic/doc" -c q
+
+# Install easymotion
+git clone https://github.com/easymotion/vim-easymotion ~/.vim/bundle/vim-easymotion
+vim -u NONE -c "helptags ~/.vim/bundle/vim-easymotion/doc" -c q
 
