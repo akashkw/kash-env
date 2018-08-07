fde641d59f9b73999fcdfacd0a7f0e09853333de
akashkw <akashkw@gmail.com>
2018-08-06 22:13:08 -0500

    minor bugfix

--- misc/install_chrome.sh
+++ misc/install_chrome.sh
@@ -5,6 +5,7 @@ set -x
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 cd $SCRIPT_PATH
 
+sudo rm /etc/apt/sources.list.d/google-chrome.list
 wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 sudo sed -i '\_http://dl.google.com/linux/chrome/deb/ stable main_d' /etc/apt/sources.list.d/google.list
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
