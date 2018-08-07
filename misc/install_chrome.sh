aa7e6526bb9b50645d2438fcfce26ba05ee3b63c
akashkw <akashkw@gmail.com>
2018-08-07 10:24:17 -0500

    spotify script

--- misc/install_chrome.sh
+++ misc/install_chrome.sh
@@ -10,5 +10,5 @@ wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-ke
 sudo sed -i '\_http://dl.google.com/linux/chrome/deb/ stable main_d' /etc/apt/sources.list.d/google.list
 sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
 sudo apt-get update
-sudo apt-get install google-chrome-stable
+sudo apt-get install -y google-chrome-stable
 
