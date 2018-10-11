0204e075840f8dbcd7b5a5bc0fec4143650ef061
Akash Kwatra <akashkw@gmail.com>
2018-10-10 20:07:15 -0500

    change to redshift

--- misc/install_redshift.sh
+++ misc/install_redshift.sh
@@ -5,7 +5,6 @@ set -x
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 cd $SCRIPT_PATH
 
-sudo add-apt-repository ppa:nathan-renniewaldock/flux
 sudo apt-get update
-sudo apt-get install -y fluxgui
+sudo apt-get install -y redshift-gtk
 
