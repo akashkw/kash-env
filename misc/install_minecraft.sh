a99d7344529baff5df8b0c0a4f48ee3f698c1730
akashkw <akashkw@gmail.com>
2018-12-11 18:30:42 -0600

    fix minecraft script

--- misc/install_minecraft.sh
+++ misc/install_minecraft.sh
@@ -4,8 +4,11 @@ set -x
 
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 cd $SCRIPT_PATH
+cd /tmp
 
-sudo add-apt-repository ppa:flexiondotorg/minecraft
-sudo apt-get update
-sudo apt-get install -y minecraft-installer
+sudo wget https://launcher.mojang.com/download/Minecraft.dmg
+sudo dpkg Minecraft.dmg
+sudo rm Minecraft.dmg
+minecraft
+
 
