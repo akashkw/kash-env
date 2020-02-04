048398644ac8e03074de38fa0c6f691587d710ec
Akash Kwatra <akashkw@gmail.com>
2020-02-03 20:38:39 -0600

    fix spotify

--- misc/install_spotify.sh
+++ misc/install_spotify.sh
@@ -5,9 +5,8 @@ set -x
 SCRIPT_PATH=$(dirname $(readlink -f $0))
 cd $SCRIPT_PATH
 
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 931FF8E79F0876134EDDBDCCA87FF9DF48BF1C90
-sudo sed -i '\_http://repository.spotify.com stable non-free_d' /etc/apt/sources.list.d/spotify.list
-echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
-sudo apt-get update
-sudo apt-get install -y spotify-client
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+sudo apt-get update && sudo apt-get install spotify-client
+
 
