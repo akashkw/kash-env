d63d62c5220a3ea8cea04b59ccc4266c8c5a016e
Akash Kwatra <akashkw@gmail.com>
2020-02-03 20:36:40 -0600

    fix vscode install

--- misc/install_vscode.sh
+++ misc/install_vscode.sh
@@ -6,7 +6,9 @@ SCRIPT_PATH=$(dirname $(readlink -f $0))
 cd $SCRIPT_PATH
 cd /tmp
 
-sudo wget https://go.microsoft.com/fwlink/?LinkID=760868
-sudo dpkg -i code*.deb
-sudo apt-get install -f -y
-sudo rm code*.deb
+curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+sudo apt install -y apt-transport-https
+sudo apt update
+sudo apt install code
